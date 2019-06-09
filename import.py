bf09e4d17dae8233e20ca0cbb7927061cbb83591
Ruoshui <ruoshuim@gmail.com>
2019-06-08 20:06:52 -0400

    Modify import to use env for database

--- import.py
+++ import.py
@@ -1,3 +1,4 @@
+from os import getenv
 import psycopg2
 from sqlalchemy import create_engine
 import csv
@@ -5,8 +6,11 @@ import csv
 # local test database 
 # engine = create_engine('postgresql://postgres:postgres@localhost:5432/project1')
 
+if not getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
 # actual database
-engine = create_engine("postgres://jgiduehaaiifrf:c8f46939b0036d5279517bd6e058b003814d303246b41a14eab3b0a066331453@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d4jthl04loj6n1")
+engine = create_engine(getenv("DATABASE_URL"))
 
 db = engine.connect()
 
