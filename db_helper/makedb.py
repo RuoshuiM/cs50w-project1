53fd37580d93ef2b0b3eea77792a3c0df16479e0
Ruoshui <ruoshuim@gmail.com>
2019-06-09 19:33:56 -0400

    Adds helpers for database

---
+++ db_helper/makedb.py
@@ -0,0 +1,52 @@
+from os import getenv
+import psycopg2
+from sqlalchemy import create_engine
+from sys import argv
+
+# local test database 
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/project1')
+
+# if not getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# # actual database
+# engine = create_engine(getenv("DATABASE_URL"))
+
+db = engine.connect()
+
+def create():    
+    # table of users
+    db.execute("""CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR NOT NULL,
+        passhash VARCHAR NOT NULL
+        )""")
+
+    db.execute("""CREATE TABLE reviews (
+        user_id INTEGER REFERENCES users(id),
+        book_id INTEGER REFERENCES books(id),
+        rating INTEGER NOT NULL,
+        text VARCHAR NOT NULL,
+        PRIMARY KEY (user_id, book_id)
+    )""")
+
+def get(sql):
+    result = db.execute(sql)
+    return result.fetchall()
+
+# def drop(table):
+#     db.execute("DROP TABLE users")
+
+if __name__ == "__main__":
+    if len(argv) <= 1:
+        print("Usage: get/create")
+        exit(1)
+    if argv[1] == "get":
+        print(get(argv[2]))
+    elif argv[1] == "create":
+        create()
+    # elif argv[1] == "drop":
+    #     drop(argv[2])
+    else:
+        print(argv)
+
